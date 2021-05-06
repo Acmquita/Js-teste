1a6ad7322db38b3e64379c1a9a1d15010f148d4d
Acmquita <acmesquita19@gmail.com>
2021-05-06 19:11:05 -0300

    Labuta

---
+++ main.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains('diminuir')){
+            count--;
+        }
+        else if (styles.contains('aumentar')){
+            count++;
+        }
+        else{
+            count=0;
+        }
+        value.textContent=count;
+    });
+});
